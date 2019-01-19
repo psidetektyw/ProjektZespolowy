97ace7b470eef3381b4ce6c07027557f64eaaa1c
dk22ino <43841408+dk22ino@users.noreply.github.com>
2019-01-19 11:29:10 +0100

    project

---
+++ Schronisko/Schronisko/Controllers/AdminController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Schronisko.Helpers;
+using Schronisko.Models;
+
+
+namespace Schronisko.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+  
+
+        [HttpGet]
+        public ActionResult UsersList()
+        {
+            if (UserHelper.GetUserRole(User.Identity.Name) != "admin") return RedirectToAction("logowanie", "Account");
+            pszczupakEntities ent = new pszczupakEntities();
+
+            List<UserViewModel> users = new List<UserViewModel>();
+
+            foreach (Users u in ent.Users.ToList())
+            {
+                UserViewModel p = new UserViewModel(u);
+
+                users.Add(p);
+
+            }
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult UsersRole()
+        {
+            if (UserHelper.GetUserRole(User.Identity.Name) != "admin") return RedirectToAction("logowanie", "Account");
+            pszczupakEntities ent = new pszczupakEntities();
+
+            List<UserViewModel> users = new List<UserViewModel>();
+
+            foreach (Users u in ent.Users.ToList())
+            {
+                UserViewModel p = new UserViewModel(u);
+
+
+                if (User.Identity.Name != p.login) { users.Add(p); }
+                //  users.Add(p);
+
+            }
+
+            return View(users);
+
+        }
+
+        [HttpGet]
+        public ActionResult EditRole(int id)
+        {
+            if (UserHelper.GetUserRole(User.Identity.Name) != "admin") return RedirectToAction("logowanie", "Account");
+            pszczupakEntities ent = new pszczupakEntities();
+
+            
+      
+
+
+            var b = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "admin", Value = "admin"},
+                new SelectListItem { Text = "manager", Value = "manager"},
+                new SelectListItem { Text = "worker", Value = "worker"},
+                new SelectListItem { Text = "user", Value = "user"}
+            };
+
+            ViewData["Roles"] = b;
+
+            
+
+            Users u = ent.Users.Where(x => x.id == id).FirstOrDefault();
+            EditRoleModel user = new EditRoleModel();
+
+            ViewBag.login  = u.login;
+
+            user.id = id;
+            user.role = u.role;
+      
+
+            return View(user);
+
+        }
+
+
+        [HttpPost]
+
+        public ActionResult EditRole(EditRoleModel user)
+        {
+            if (UserHelper.GetUserRole(User.Identity.Name) != "admin") return RedirectToAction("Index", "Home");
+            if (ModelState.IsValid)
+            {
+                pszczupakEntities ent = new pszczupakEntities();
+                Users u = new Users();
+                u = ent.Users.Where(x => x.id == user.id).FirstOrDefault();
+
+  
+                u.role = user.role;
+         
+
+
+                ent.Entry(ent.Users.Where(x => x.id == u.id).First()).CurrentValues.SetValues(u);
+                ent.SaveChanges();
+                return RedirectToAction("UsersRole");
+            }
+            else
+            {
+
+
+                pszczupakEntities ent = new pszczupakEntities();
+          
+                Users u = ent.Users.Where(x => x.id == user.id).FirstOrDefault();
+
+                var b = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "admin", Value = "admin"},
+                new SelectListItem { Text = "manager", Value = "manager"},
+                new SelectListItem { Text = "worker", Value = "worker"},
+                new SelectListItem { Text = "user", Value = "user"}
+            };
+
+                ViewData["Roles"] = b;
+                ViewBag.login = u.login;
+
+                return View(user);
+            }
+
+
+
+
+        }
+
+
+        [HttpGet]
+       public ActionResult DeleteUser(int id) {
+            if (UserHelper.GetUserRole(User.Identity.Name) != "admin") return RedirectToAction("logowanie", "Account");
+            pszczupakEntities ent = new pszczupakEntities();
+            Users user = ent.Users.Where(x => x.id == id).First();
+            ent.Users.Remove(user);
+            ent.SaveChanges();
+
+
+            return RedirectToAction("UsersRole");
+        }
+
+
+    }
+}
---
+++ Schronisko/Schronisko/Validators/User/PESEL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+
+
+namespace Schronisko.Validators.User
+{
+    public class PESEL : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            var pesel = value.ToString();
+            if (Regex.IsMatch(pesel, "^[0-9]{11}$"))
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Invalid length PESEL (must be 11 characters).");
+        }
+    }
+}

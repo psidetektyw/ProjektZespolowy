13e2a6215e0f3a6e96cdda1f76c9c29c6c5fa279
Paulina Żurawska <zurawskapa@gmail.com>
2019-01-19 12:13:15 +0100

    project

---
+++ Schronisko/Schronisko/Models/EditUserModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Schronisko.Validators.User;
+
+
+namespace Schronisko.Models
+{
+    public class EditUserModel
+    {
+        public int id { get; set; }
+
+
+        [NAZWY]
+        [StringLength(20, ErrorMessage = "{0} musi mieć minimum {2} a maksymalnie {1}", MinimumLength = 2)]
+        [DisplayName(displayName: "Imię")]
+        public string name { get; set; }
+
+
+        [NAZWY]
+        [StringLength(25, ErrorMessage = "{0} musi mieć minimum {2} a maksymalnie {1}", MinimumLength = 2)]
+        [DisplayName(displayName: "Nazwisko")]
+        public string surname { get; set; }
+
+
+        [PESEL]
+        [DisplayName(displayName: "Pesel")]
+        public string pesel { get; set; }
+
+
+        [NAZWY]
+        [StringLength(30, ErrorMessage = "{0} musi mieć minimum {2} a maksymalnie {1}", MinimumLength = 3)]
+        [DisplayName(displayName: "Miasto")]
+        public string city { get; set; }
+
+
+        [DisplayName(displayName: "Ulica")]
+        public string street { get; set; }
+
+
+        [DisplayName(displayName: "Numer")]
+        public string house { get; set; }
+
+
+        [DisplayName(displayName: "Numer telefonu")]
+        public string phone { get; set; }
+    }
+}
---
+++ Schronisko/Schronisko/Models/RegisterModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Schronisko.Validators.User;
+using Schronisko.Validators;
+
+namespace Schronisko.Models
+{
+
+    [PASSWORD("password", "confirmPassword", ErrorMessage = "Hasła się nie zgadzają")]
+    public class RegisterModel
+    {
+        [Required]
+        [LOGIN]
+        [DisplayName(displayName: "Login")]
+        public string login { get; set; }
+
+        [Required]
+        [EMAIL]
+        [EmailAddress]
+        [DisplayName(displayName: "Email")]
+        public string email { get; set; }
+
+
+        [Required]
+        [StringLength(12, ErrorMessage = "{0} musi mieć minimum {2} a maksymalnie {1}", MinimumLength = 6)]
+        [DisplayName(displayName: "Hasło")]
+        public string password { get; set; }
+
+
+        [Required]
+        [DisplayName(displayName: "Potwierdz Hasło")]
+        public string confirmPassword { get; set; }
+
+
+    }
+}
